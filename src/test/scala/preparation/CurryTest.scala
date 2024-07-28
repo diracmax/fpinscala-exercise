fd36f69454d2f4312f93b5b0fb2c8df8af2b57c0
diracmax <mkazuki0705@gmail.com>
2024-07-28 10:31:23 +0900

    【EXERCISE 2.3】カリー化できる

---
+++ src/test/scala/preparation/CurryTest.scala
@@ -0,0 +1,11 @@
+package preparation
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class CurryTest extends AnyFunSuite{
+  test("Curry.curry should return curried function") {
+    val f = (a: Int, b: Int) => a + b
+    val curried = Curry.curry(f)
+    assert(curried(1)(2) == 3)
+  }
+}
