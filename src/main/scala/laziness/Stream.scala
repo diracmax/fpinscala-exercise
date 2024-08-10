8bb9132eb123af3818ddc187d7613cad68d1ea98
diracmax <mkazuki0705@gmail.com>
2024-08-10 21:51:23 +0900

    【EXERCISE 5.2】Stream.take and Stream.drop

--- src/main/scala/laziness/Stream.scala
+++ src/main/scala/laziness/Stream.scala
@@ -7,6 +7,16 @@ sealed trait Stream[+A] {
     case Empty => Nil
     case Cons(h, t) => h() :: t().toList
   }
+
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 0 => Cons(h, () => t().take(n - 1))
+    case _ => Empty
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ => this
+  }
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
--- src/test/scala/laziness/StreamTest.scala
+++ src/test/scala/laziness/StreamTest.scala
@@ -8,4 +8,16 @@ class StreamTest extends AnyFunSuite {
     assert(Stream(1, 2, 3).toList == List(1, 2, 3))
     assert(Stream().toList == Nil)
   }
+
+  test("Exercise 5.2 take should return the first n values") {
+    assert(Stream(1, 2, 3).take(2).toList == List(1, 2))
+    assert(Stream(1, 2, 3).take(4).toList == List(1, 2, 3))
+    assert(Stream(1, 2, 3).take(0).toList == Nil)
+  }
+
+  test("Exercise 5.2 drop should return the stream without the first n values") {
+    assert(Stream(1, 2, 3).drop(2).toList == List(3))
+    assert(Stream(1, 2, 3).drop(4).toList == Nil)
+    assert(Stream(1, 2, 3).drop(0).toList == List(1, 2, 3))
+  }
 }
